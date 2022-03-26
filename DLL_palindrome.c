820242b372883abe2a43e67ac22e08a6b011c415
Saleeza <102399258+Saleeza@users.noreply.github.com>
2022-03-26 10:59:20 +0530

    Add files via upload

---
+++ DLL_palindrome.c
@@ -0,0 +1,174 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+typedef struct dlist
+{
+    int data;
+    struct dlist *link;
+    struct dlist *prev;
+} node ;
+void create(node **end,node **head)
+{
+    int item,ch=1,choice;
+    if (*head!=NULL)
+    {
+        printf("\n-----list already exists----");
+    }
+     node *temp=(node*)malloc(sizeof(node));
+
+    while(ch==1)
+    {
+
+    printf("\n\tEnter the element to be inserted : ");
+    scanf("%d",&item);
+    node *new=(node*)malloc(sizeof(node));
+    new->data=item;
+    new->link=NULL;
+    new->prev=NULL;
+
+    if (*head==NULL)
+    {
+        temp=*head=*end=new;
+    }
+    else
+    {
+     temp->link=new;
+     new->prev=temp;
+    temp=new;
+    }
+    *end=new;
+    printf("\n\tdo you want to continue  1)yes    2)no  ");
+    scanf("%d",&choice);
+    ch=choice;
+}
+}
+ void delstart(node **head)
+ {
+     node *temp;
+     temp=*head;
+    temp=temp->link;
+    temp->prev=NULL;
+    *head=temp;
+
+     return ;
+ }
+ void dellast(node **end)
+ {
+     node *temp;
+     temp=*end;
+     temp->prev->link=NULL;
+     *end=temp->prev;
+     
+ }
+
+
+void del(int item,node *head,node *end)
+{
+    node *temp1,*temp;
+    for(temp=head;((temp->data!=item) && (temp!=NULL));temp=temp->link)
+    {
+        temp1=temp; 
+    }
+    if (temp1==head)
+    {
+        delstart(&head);
+    }
+    else
+    {
+        if(temp1->link->link==NULL)
+        {
+           dellast(&end) ;
+        }
+        else
+        {
+            temp1->link=temp1->link->link;
+            temp1->link->link->prev=temp1;
+
+        }
+    }
+
+
+}
+ void delpalin(node *head,node *end)
+{ 
+     node *temp;
+     int r,rev,item;
+     temp=head;
+     item=temp->data;
+    while(temp->data!=0)
+    {
+        r=temp->data%10;
+        rev=rev*10+r;
+        temp->data=temp->data/10;
+    }
+
+    if(rev==item)
+    {
+        delstart(&head);
+    }
+}
+void display(node *head,node *end)
+{
+    node *temp;
+    printf("\n\t prev address   address   data    new address");
+    for (temp=head;temp!=NULL;temp=temp->link)
+    {
+        printf("\n\t%10u   %10u   %d    %10u",temp->prev,temp,temp->data,temp->link);
+    }
+
+     printf("\n\t==================================================");
+     printf("\n\t prev address   address   data    new address");
+    for (temp=end;temp!=NULL;temp=temp->prev)
+    {
+        printf("\n\t%10u   %10u   %d    %10u",temp->prev,temp,temp->data,temp->link);
+    }
+    return ;
+}
+int main()
+{
+    
+    int choice,item;
+    node* head= ( node*)malloc(sizeof( node));
+     node* end= ( node*)malloc(sizeof( node));
+    end=head=NULL;
+     while (1)
+     {
+      printf("\n\t\t-----------main menu-------\n");
+     printf("\t 1) create a doubly linkedlist\n");
+     printf("\t 2) display \n");
+     printf("\t 3) delete palindromes \n ");
+     printf("\t 4) delete first \n ");
+     printf("\t 5) delete last node \n ");
+     printf("\t 6) delete any element\n ");
+     printf("\t 7) exit\n");
+
+     printf("\tenter the choice  ");
+
+     scanf("%d",&choice);
+     switch(choice)
+      {
+        case 1:
+           create(&end,&head);
+           break;
+        case 2:
+           display(head,end);
+           break;
+        case 3:
+            delpalin(head,end);
+            break;
+        case 4:
+           delstart(&head);
+           break;
+        case 5:
+           dellast(&end);
+           break;
+        case 6:
+        printf("node we want to delete ");
+        scanf("%d",&item);
+           del(item,head,end);
+           break;
+        case 7:
+          exit(0);
+      }
+     }
+    }
